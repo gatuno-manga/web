36e9dadb135ddf68dbb4711ed891db5ed089f0cf
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-07-10 02:29:08 -0300

    feat: page chapters

--- src/app/pages/chapters/chapters.component.ts
+++ src/app/pages/chapters/chapters.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Chapter } from '../../models/book.models';
 import { BookService } from '../../service/book.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { IconsComponent } from '../../components/icons/icons.component';
 import { HeaderComponent } from '../../components/header/header.component';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-chapters',
-  imports: [IconsComponent, HeaderComponent, NgIf, RouterModule],
+  imports: [IconsComponent, HeaderComponent, NgIf, RouterModule, NgClass],
   templateUrl: './chapters.component.html',
   styleUrl: './chapters.component.scss'
 })
 export class ChaptersComponent {
   chapter!: Chapter;
+  showBtnTop = false;
+  private lastScrollTop = 0;
+  private scrollThreshold = 1500;
 
   constructor(
     private bookService: BookService,
@@ -36,17 +39,46 @@ export class ChaptersComponent {
   }
 
   backPage() {
-    this.router.navigate(['../'], { relativeTo: this.activatedRoute });
+    this.router.navigate(['../'], { relativeTo: this.activatedRoute }).then(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
   }
 
   nextPage() {
     if (this.chapter.next) {
-      this.router.navigate(['../', this.chapter.next], { relativeTo: this.activatedRoute });
+      this.router.navigate(['../', this.chapter.next], { relativeTo: this.activatedRoute }).then(() => {
+        window.scrollTo({ top: 0 });
+      });
     }
   }
   previousPage() {
     if (this.chapter.previous) {
-      this.router.navigate(['../', this.chapter.previous], { relativeTo: this.activatedRoute });
+      this.router.navigate(['../', this.chapter.previous], { relativeTo: this.activatedRoute }).then(() => {
+        window.scrollTo({ top: 0 });
+      });
     }
+  }
+
+  scrolledUpAmount = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st < this.lastScrollTop) {
+      this.scrolledUpAmount += this.lastScrollTop - st;
+    } else {
+      this.scrolledUpAmount = 0;
+    }
+    this.showBtnTop = this.scrolledUpAmount > this.scrollThreshold;
+    this.lastScrollTop = st <= 0 ? 0 : st;
+  }
+
+  clearScrolledUpAmount() {
+    this.scrolledUpAmount = 0;
+    this.showBtnTop = false;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
