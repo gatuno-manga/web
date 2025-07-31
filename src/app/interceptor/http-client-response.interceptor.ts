f5e656dbd29b764fe51fd6e0515b2b740f8e7c73
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-07-31 00:47:44 -0300

    refacture

--- src/app/interceptor/http-client-response.interceptor.ts
+++ src/app/interceptor/http-client-response.interceptor.ts
@@ -4,32 +4,37 @@ import { catchError, switchMap, throwError } from 'rxjs';
 import { UserTokenService } from '../service/user-token.service';
 
 export const httpClientResponseInterceptor: HttpInterceptorFn = (req, next) => {
-  const userTokenService = inject(UserTokenService);
-  const token = userTokenService.AccessToken;
+	const userTokenService = inject(UserTokenService);
+	const token = userTokenService.AccessToken;
 
-  let clonedRequest = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+	let clonedRequest = req.clone({
+		setHeaders: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
 
-  return next(clonedRequest).pipe(
-    catchError((error) => {
-      if (error.status === 401 && !req.url.includes('/auth/refresh') && userTokenService.hasToken) {
-        return userTokenService.refreshTokens().pipe(
-          switchMap((newToken) => {
-            userTokenService.setTokens(newToken.accessToken, newToken.refreshToken);
-            const retryRequest = req.clone({
-              setHeaders: {
-                Authorization: `Bearer ${newToken}`,
-              },
-            });
-            return next(retryRequest);
-          }),
-          catchError(() => throwError(() => error))
-        );
-      }
-      return throwError(() => error);
-    })
-  );
+	const requestExclude = ['/auth/signup'];
+	return next(clonedRequest).pipe(
+		catchError((error) => {
+			if (
+				error.status === 401 &&
+				userTokenService.hasToken &&
+				!requestExclude.some(url => req.url.includes(url))
+			) {
+				return userTokenService.refreshTokens().pipe(
+					switchMap((newToken) => {
+						userTokenService.setTokens(newToken.accessToken, newToken.refreshToken);
+						const retryRequest = req.clone({
+							setHeaders: {
+								Authorization: `Bearer ${newToken}`,
+							},
+						});
+						return next(retryRequest);
+					}),
+					catchError(() => throwError(() => error))
+				);
+			}
+			return throwError(() => error);
+		})
+	);
 };
