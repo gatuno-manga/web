7b1cbe481d82b092d9dceaff061219ce330fe121
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-09-02 11:58:54 -0300

    fix: notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './service/theme.service';
 import { SensitiveContentService } from './service/sensitive-content.service';
+import { MetaDataService } from './service/meta-data.service';
+import { OverlayNotificationComponent } from './components/notification/overlay-notification/overlay-notification.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OverlayNotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
