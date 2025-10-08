720d105a06add1e62d6871ac0cb45bb3b5adb6ae
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-10-08 01:29:26 -0300

    feat: add support for advanced filters in book listing

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { LocalStorageService } from '../../service/local-storage.service';
 import { BookService } from '../../service/book.service';
-import { BookList } from '../../models/book.models';
-import { RouterModule } from '@angular/router';
+import { BookList, BookPageOptions } from '../../models/book.models';
+import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { ItemBookComponent } from '../../components/item-book/item-book.component';
 import { Page } from '../../models/miscellaneous.models';
 import { SelectComponent } from '../../components/select/select.component';
@@ -20,6 +20,7 @@ export class BooksComponent {
   pagesToShow: number[] = [];
   isLoading = signal(true);
   bookOptions: 'grid' | 'list' = 'grid';
+  filterOptions: BookPageOptions = {};
   selectList = [
     {
       icon: 'grid',
@@ -33,13 +34,40 @@ export class BooksComponent {
 
   constructor(
     private booksService: BookService,
-    private localStorage: LocalStorageService
+    private localStorage: LocalStorageService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     const savedLayout = this.localStorage.get('books-layout');
     if (savedLayout === 'grid' || savedLayout === 'list') {
       this.bookOptions = savedLayout;
     }
-    this.loadBooks(this.currentPage);
+
+    this.route.queryParams.subscribe(params => {
+      const pageFromUrl = params['page'] ? parseInt(params['page'], 10) : 1;
+      this.currentPage = pageFromUrl > 0 ? pageFromUrl : 1;
+
+      const filters: BookPageOptions = {
+        page: this.currentPage,
+      };
+
+      if (params['search']) filters.search = params['search'];
+      if (params['type']) filters.type = Array.isArray(params['type']) ? params['type'] : [params['type']];
+      if (params['tags']) filters.tags = Array.isArray(params['tags']) ? params['tags'] : [params['tags']];
+      if (params['tagsLogic']) filters.tagsLogic = params['tagsLogic'] as 'and' | 'or';
+      if (params['excludeTags']) filters.excludeTags = Array.isArray(params['excludeTags']) ? params['excludeTags'] : [params['excludeTags']];
+      if (params['excludeTagsLogic']) filters.excludeTagsLogic = params['excludeTagsLogic'] as 'and' | 'or';
+      if (params['authors']) filters.authors = Array.isArray(params['authors']) ? params['authors'] : [params['authors']];
+      if (params['authorsLogic']) filters.authorsLogic = params['authorsLogic'] as 'and' | 'or';
+      if (params['publication']) filters.publication = parseInt(params['publication'], 10);
+      if (params['publicationOperator']) filters.publicationOperator = params['publicationOperator'] as 'eq' | 'gt' | 'lt' | 'gte' | 'lte';
+      if (params['orderBy']) filters.orderBy = params['orderBy'] as 'title' | 'createdAt' | 'updatedAt' | 'publication';
+      if (params['order']) filters.order = params['order'] as 'ASC' | 'DESC';
+      if (params['sensitiveContent']) filters.sensitiveContent = Array.isArray(params['sensitiveContent']) ? params['sensitiveContent'] : [params['sensitiveContent']];
+
+      this.filterOptions = filters;
+      this.loadBooks();
+    });
   }
 
   setBookOptions(option: 'grid' | 'list') {
@@ -47,8 +75,8 @@ export class BooksComponent {
     this.localStorage.set('books-layout', option);
   }
 
-  loadBooks(page: number) {
-    this.booksService.getBooks({ page }).subscribe((bookPage: Page<BookList>) => {
+  loadBooks() {
+    this.booksService.getBooks(this.filterOptions).subscribe((bookPage: Page<BookList>) => {
       this.books = bookPage.data;
       this.currentPage = bookPage.metadata.page;
       this.lastPage = bookPage.metadata.lastPage;
@@ -73,7 +101,14 @@ export class BooksComponent {
 
   goToPage(page: number) {
     if (page !== this.currentPage && page >= 1 && page <= this.lastPage) {
-      this.loadBooks(page);
+      const cleanParams = Object.fromEntries(
+        Object.entries({ ...this.filterOptions, page }).filter(([_, value]) => value !== undefined)
+      );
+
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: cleanParams
+      });
     }
   }
 
