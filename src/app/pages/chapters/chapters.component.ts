dd97abdff40d920407de0df158278c061a85ad5c
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-10-13 21:54:31 -0300

    feat: adds support for long titles and adjusts the layout of the book component

--- src/app/pages/chapters/chapters.component.ts
+++ src/app/pages/chapters/chapters.component.ts
@@ -4,19 +4,22 @@ import { BookService } from '../../service/book.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { IconsComponent } from '../../components/icons/icons.component';
 import { HeaderComponent } from '../../components/header/header.component';
-import { DecimalPipe, NgClass, NgIf } from '@angular/common';
+import { DecimalPipe, NgClass } from '@angular/common';
 import { ChapterService } from '../../service/chapter.service';
 import { UserTokenService } from '../../service/user-token.service';
 import { ModalNotificationService } from '../../service/modal-notification.service';
+import { ButtonComponent } from '../../components/inputs/button/button.component';
+import { AsideComponent } from '../../components/aside/aside.component';
+import { MetaDataService } from '../../service/meta-data.service';
 
 @Component({
   selector: 'app-chapters',
-  imports: [IconsComponent, HeaderComponent, NgIf, RouterModule, NgClass, DecimalPipe],
+  imports: [IconsComponent, HeaderComponent, RouterModule, NgClass, DecimalPipe, ButtonComponent, AsideComponent],
   templateUrl: './chapters.component.html',
   styleUrl: './chapters.component.scss'
 })
 export class ChaptersComponent {
-  chapter!: Chapter;
+  chapter?: Chapter;
   showBtnTop = false;
   private lastScrollTop = 0;
   private scrollThreshold = 1500;
@@ -27,6 +30,7 @@ export class ChaptersComponent {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private userTokenService: UserTokenService,
+    private metaService: MetaDataService,
     private modalService: ModalNotificationService
   ) {
     this.admin = this.userTokenService.isAdmin();
@@ -42,8 +46,24 @@ export class ChaptersComponent {
       }
       this.chapterService.getChapter(chapter).subscribe((chapter: Chapter) => {
         this.chapter = chapter;
+        this.setMetaData();
       });
     });
+  }
+
+  private formatNumber(value: number): string {
+    return Number(value).toLocaleString('pt-BR', {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    });
+  }
+
+  setMetaData() {
+    this.metaService.setMetaData({
+      title: this.chapter ? `Capitulo ${this.formatNumber(this.chapter.index)} | ${this.chapter.bookTitle}` : 'Capítulo',
+      description: this.chapter ? `Ler online o capítulo ${this.formatNumber(this.chapter.index)} do livro ${this.chapter.bookTitle}.` : 'Capítulo',
+      image: this.chapter ? this.chapter.pages[0].path : '',
+    })
   }
 
   backPage() {
@@ -53,14 +73,14 @@ export class ChaptersComponent {
   }
 
   nextPage() {
-    if (this.chapter.next) {
+    if (this.chapter?.next) {
       this.router.navigate(['../', this.chapter.next], { relativeTo: this.activatedRoute }).then(() => {
         window.scrollTo({ top: 0 });
       });
     }
   }
   previousPage() {
-    if (this.chapter.previous) {
+    if (this.chapter?.previous) {
       this.router.navigate(['../', this.chapter.previous], { relativeTo: this.activatedRoute }).then(() => {
         window.scrollTo({ top: 0 });
       });
@@ -148,7 +168,7 @@ export class ChaptersComponent {
     if (this.chapter) {
       this.modalService.show(
         'Redefinir Capítulo',
-        `Tem certeza que deseja redefinir o capítulo\n"${this.chapter.bookTitle} (${this.chapter.index})"?\nEsta ação não pode ser desfeita.`,
+        `Tem certeza que deseja redefinir o capítulo\n"${this.chapter.bookTitle} (${this.formatNumber(this.chapter.index)})"?\nEsta ação não pode ser desfeita.`,
         [
           {
             label: 'Cancelar',
@@ -171,6 +191,12 @@ export class ChaptersComponent {
       this.chapterService.resetChapter(this.chapter.id).subscribe(() => {
         this.router.navigate(['../'], { relativeTo: this.activatedRoute });
       });
+    }
+  }
+
+  openOriginalLink() {
+    if (this.chapter && this.chapter.originalUrl) {
+      window.open(this.chapter.originalUrl, '_blank');
     }
   }
 }
