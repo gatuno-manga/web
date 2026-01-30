9b9d5b644b596160f865273398db3454e9f7b246
luis henrique <luishenrique.gbarbosa21@gmail.com>
2026-01-30 16:59:29 -0300

    feat: refactor header and outlet components for consistent change detection strategy

--- src/app/pages/outlet/defalt-outlet/defalt-outlet.component.ts
+++ src/app/pages/outlet/defalt-outlet/defalt-outlet.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-defalt-outlet',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [HeaderComponent, RouterModule, RouterOutlet],
   templateUrl: './defalt-outlet.component.html',
   styleUrl: './defalt-outlet.component.scss'
