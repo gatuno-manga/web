02e1292306203623f95b86ec6d39f3949d3e2361
luis henrique <luishenrique.gbarbosa21@gmail.com>
2026-02-07 21:46:13 -0300

    fix: correct API URLs to use HTTP in environment files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiURL: 'https://localhost:3000/api',
-    apiURLServer: 'https://api:3000/api',
+    apiURL: 'http://localhost:3000/api',
+    apiURLServer: 'http://api:3000/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiURL: 'https://localhost:3001/api',
-    apiURLServer: 'https://api:3000/api',
+    apiURL: 'http://localhost:3001/api',
+    apiURLServer: 'http://api:3000/api',
 };
