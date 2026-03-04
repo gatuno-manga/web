a002bc0e38c512b4cdfc2f94744d1abcc3ab96fd
Luis Henrique G Barbosa <92602646+BGLuis@users.noreply.github.com>
2026-03-04 18:45:58 -0300

    Update apiURL to use internal API endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiURL: 'https://api-gatuno.barbosa.local/api',
+    apiURL: 'https://api-gatuno.barbosa.internal/api',
     apiURLServer: 'https://api:3000/api',
 };
