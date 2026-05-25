91375a2754bdceb9384f3c1f5b024374e3b7ebf8
luis henrique <luishenrique.gbarbosa21@gmail.com>
2026-05-24 22:52:39 -0300

    feat: update multi-select tags component to use app-button and improve input handling

--- src/app/shared/ui/organisms/multi-select-tags/multi-select-tags.component.ts
+++ src/app/shared/ui/organisms/multi-select-tags/multi-select-tags.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IconsComponent } from '@ui/atoms/icons/icons.component';
+import { ButtonComponent } from '@ui/atoms/inputs/button/button.component';
 import { TextInputComponent } from '@ui/atoms/inputs/text-input/text-input.component';
 
 export interface MultiSelectOption {
@@ -19,7 +20,13 @@ export interface MultiSelectOption {
 @Component({
 	selector: 'app-multi-select-tags',
 	standalone: true,
-	imports: [CommonModule, FormsModule, TextInputComponent, IconsComponent],
+	imports: [
+		CommonModule,
+		FormsModule,
+		TextInputComponent,
+		IconsComponent,
+		ButtonComponent,
+	],
 	templateUrl: './multi-select-tags.component.html',
 	styleUrl: './multi-select-tags.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
