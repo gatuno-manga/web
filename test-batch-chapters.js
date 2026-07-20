0768217dc050646bb0284b3a5f115cacc7f95ead
luis henrique <luishenrique.gbarbosa21@gmail.com>
2026-07-20 05:02:01 -0300

    Remove update-latest-reads.js script that injected ChapterService and modified loadHistory method in latest-reads.component.ts

--- test-batch-chapters.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:3000/api/chapters/batch/data', {
-	method: 'POST',
-	headers: { 'Content-Type': 'application/json' },
-	body: JSON.stringify(['some-chapter-id']),
-})
-	.then((r) => r.json())
-	.then(console.log)
-	.catch(console.error);
--- test-rest-all.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:3000/api/books?limit=5')
-	.then((r) => r.json())
-	.then((data) => {
-		if (data.data) {
-			console.log(Object.keys(data.data[0]));
-		}
-	})
-	.catch(console.error);
