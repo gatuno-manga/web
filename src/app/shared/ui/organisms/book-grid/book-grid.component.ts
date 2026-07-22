14dfedae629c2cf30cac0cdc14178e355880d393
luis henrique <luishenrique.gbarbosa21@gmail.com>
2026-07-22 06:18:20 -0300

    feat: refactor book grid component for improved skeleton loading and styling

--- src/app/shared/ui/organisms/book-grid/book-grid.component.ts
+++ src/app/shared/ui/organisms/book-grid/book-grid.component.ts
@@ -1,12 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, computed } from '@angular/core';
 import { ItemBookComponent } from '@features/books/components/item-book/item-book.component';
 import { BookList } from '@models/book.models';
+import { IconsComponent } from '@ui/atoms/icons/icons.component';
 
 @Component({
 	selector: 'app-book-grid',
 	standalone: true,
-	imports: [CommonModule, ItemBookComponent],
+	imports: [ItemBookComponent, IconsComponent],
 	templateUrl: './book-grid.component.html',
 	styleUrl: './book-grid.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,4 +16,6 @@ export class BookGridComponent {
 	isLoading = input<boolean>(false);
 	type = input<'grid' | 'list' | 'cover'>('grid');
 	skeletonCount = input<number>(10);
+
+	skeletons = computed(() => new Array(this.skeletonCount()).fill(0));
 }
